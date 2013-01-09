ef45c73815e2007246a6d9515578fd0c1633bf9a
NShmakov <nshmakov@yotateam.com>
2013-01-09 16:43:09 +0400

    change port to 8080

--- java-sockets-example/src/main/java/com/blogspot/nikcode/socket/Server.java
+++ java-sockets-example/src/main/java/com/blogspot/nikcode/socket/Server.java
@@ -13,7 +13,7 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(6666);
+        ServerSocket serverSocket = new ServerSocket(8080);
         Socket socket = serverSocket.accept();
         InputStream inputStream = socket.getInputStream();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
