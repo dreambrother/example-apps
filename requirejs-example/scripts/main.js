4ec42d549428bd338f8e77d82c15f6033ae555f9
NShmakov <nshmakov@yotateam.com>
2013-03-20 13:58:55 +0400

    add some features to requirejs-example

--- requirejs-example/scripts/main.js
+++ requirejs-example/scripts/main.js
@@ -1,5 +1,12 @@
-require(['module-one', 'module-two'], function(mOne, mTwo) {
+require.config({
+    paths: {
+        app: 'make-alias-for-this-folder'
+    }
+})
+
+require(['modules/module-one', 'modules/module-two', 'app/module-three'], function(mOne, mTwo, mThree) {
     console.log(mOne.add(1, 5))
     console.log(mOne.sub(5, 2))
     console.log(mTwo.div(10, 2))
+    console.log(mThree.mul(3, 5))
 })
---
+++ requirejs-example/scripts/make-alias-for-this-folder/module-three.js
@@ -0,0 +1,8 @@
+// defailt RequireJS module
+define(function() {
+    return {
+        mul: function(a, b) {
+            return a * b
+        }
+    }
+})
