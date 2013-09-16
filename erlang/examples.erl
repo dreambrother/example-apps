8af282d7f892243a00d57dce56983ead62d82cf6
NShmakov <nshmakov@yotateam.com>
2013-09-16 15:00:17 +0400

    add case pattern matching example

--- erlang/examples.erl
+++ erlang/examples.erl
@@ -1,5 +1,5 @@
 -module(examples).
--export([iter/1, foo/1]).
+-export([iter/1, foo/1, bar/1]).
 
 % Recursive iteration
 iter([Elem]) -> io:format("Last elem: ~p~n", [Elem]);
@@ -7,3 +7,11 @@ iter([Head|Tail]) -> io:format("~p~n", [Head]), iter(Tail).
 
 % Tuple pattern matching
 foo(A = {B, C}) -> io:format("A:~p, B: ~p, C: ~p~n", [A, B, C]).
+
+% Pattern matching in case statement
+bar(Foo) ->
+	case Foo of
+		{number, N} when N < 0 -> io:format("Negative number: ~p~n", [N]);
+		{number, N} when N >= 0 -> io:format("Positive number: ~p~n", [N]);
+		{string, N} -> io:format("String: ~s~n", [N])
+	end.
