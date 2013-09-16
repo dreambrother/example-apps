0a48ed6f307d36ae62c77990b8420d37edf97576
NShmakov <nshmakov@yotateam.com>
2013-09-16 17:57:27 +0400

    add simple interaction example

--- erlang/multiprocess_examples.erl
+++ erlang/multiprocess_examples.erl
@@ -1,6 +1,17 @@
 -module(multiprocess_examples).
--export([ten_processes/0]).
+-export([ten_processes/0, receiver/0, interaction/0]).
 
 % Process creation example
 ten_processes() ->
         lists:foreach(fun(N) -> spawn(fun() -> io:format("~p~n", [N]) end) end, lists:seq(1, 10)).
+
+% Simple interaction example
+receiver() ->
+        receive
+                Msg -> io:format("Receive message: ~p~n", [Msg]),
+                       receiver()
+        end.
+
+interaction() -> 
+        Rcvr = spawn(fun() -> receiver() end),
+        lists:foreach(fun(N) -> Rcvr ! N end, ["Foo", "Bar", "Hello, process neighbor!"]).
