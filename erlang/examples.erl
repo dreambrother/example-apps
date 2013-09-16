049e7e7b4267bf40364855ccd79c0f36458bd05f
NShmakov <nshmakov@yotateam.com>
2013-09-16 15:47:55 +0400

    add high order function example

--- erlang/examples.erl
+++ erlang/examples.erl
@@ -1,5 +1,5 @@
 -module(examples).
--export([iter/1, foo/1, bar/1]).
+-export([iter/1, foo/1, bar/1, call_func/0]).
 
 % Recursive iteration
 iter([Elem]) -> io:format("Last elem: ~p~n", [Elem]);
@@ -15,3 +15,6 @@ bar(Foo) ->
         {number, N} when N >= 0 -> io:format("Positive number: ~p~n", [N]);
         {string, N} -> io:format("String: ~s~n", [N])
     end.
+
+high_order_func(Func) -> Func({string, "Call from high order func"}).
+call_func() -> high_order_func(fun bar/1).
