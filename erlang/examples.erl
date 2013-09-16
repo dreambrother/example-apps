993389ecd7e6b88d44acb31703ad931786f578fc
NShmakov <nshmakov@yotateam.com>
2013-09-16 15:04:32 +0400

    replace tabs with 4 spaces

--- erlang/examples.erl
+++ erlang/examples.erl
@@ -10,8 +10,8 @@ foo(A = {B, C}) -> io:format("A:~p, B: ~p, C: ~p~n", [A, B, C]).
 
 % Pattern matching in case statement
 bar(Foo) ->
-	case Foo of
-		{number, N} when N < 0 -> io:format("Negative number: ~p~n", [N]);
-		{number, N} when N >= 0 -> io:format("Positive number: ~p~n", [N]);
-		{string, N} -> io:format("String: ~s~n", [N])
-	end.
+    case Foo of
+        {number, N} when N < 0 -> io:format("Negative number: ~p~n", [N]);
+        {number, N} when N >= 0 -> io:format("Positive number: ~p~n", [N]);
+        {string, N} -> io:format("String: ~s~n", [N])
+    end.
