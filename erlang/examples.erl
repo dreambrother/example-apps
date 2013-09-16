d8ecad22f07f7321f996a4068dd885e69563d282
NShmakov <nshmakov@yotateam.com>
2013-09-16 16:07:45 +0400

    add exception example

--- erlang/examples.erl
+++ erlang/examples.erl
@@ -1,5 +1,5 @@
 -module(examples).
--export([iter/1, foo/1, bar/1, call_func/0, call_anonym_func/0]).
+-export([iter/1, foo/1, bar/1, call_func/0, call_anonym_func/0, call_fun_with_exception/0]).
 
 % Recursive iteration
 iter([Elem]) -> io:format("Last elem: ~p~n", [Elem]);
@@ -22,3 +22,13 @@ call_func() -> high_order_func(fun bar/1).
 
 % Anonymous function example
 call_anonym_func() -> high_order_func(fun({X, Y}) -> io:format("~p~p~n", [X, Y]) end).
+
+% Exception example
+fun_with_try(Fun) -> 
+    try Fun() of
+        _ -> ok
+    catch
+        throw:Ex -> Ex
+    end.
+call_fun_with_exception() -> fun_with_try(fun() -> throw(some_error) end).
+
