26f5e5859b80682f29d36d2709f4106d07bacb37
dreambrother <nikshmakov@gmail.com>
2014-01-13 23:23:53 +0400

    add types sizes example

---
+++ c++/types_sizes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Types sizes in bytes: \n";
+    cout << "bool: \t\t" << sizeof(bool) << endl;
+    cout << "char: \t\t" << sizeof(char) << endl;
+    cout << "short: \t\t" << sizeof(short) << endl;
+    cout << "int: \t\t" << sizeof(int) << endl;
+    cout << "long: \t\t" << sizeof(long) << endl;
+    cout << "float: \t\t" << sizeof(float) << endl;
+    cout << "double: \t" << sizeof(double) << endl;
+}
