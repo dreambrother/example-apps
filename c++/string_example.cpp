3e13ee5989098618753edaa758c5f441f031d76f
dreambrother <nikshmakov@gmail.com>
2014-01-13 23:34:19 +0400

    strings example

---
+++ c++/string_example.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() 
+{
+    using namespace std;
+    string teststr = "I'm a string";
+    cout << teststr << endl;
+
+    string latinstr = "Латиница";
+    cout << latinstr << endl;
+}
