959e4f123cc67d9048b8c52247d91824f099f2c1
dreambrother <nikshmakov@gmail.com>
2014-01-15 01:17:50 +0400

    add arrays pointers example

--- c++/lang/pointers.cpp
+++ c++/lang/pointers.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 
-int main() 
-{
-    using namespace std;
+using namespace std;
 
+void simplePointers()
+{
     int val = 5;
     int *valRef = &val;
 
     cout << "referece: " << valRef << endl;
-    cout << "value: " << *valRef << endl;
+    cout << "value: " << *valRef << endl;    
 }
+
+void arrayPointers() 
+{
+    int arr[] = { 1, 5, 8 };
+    cout << "Dereference array: " << *arr << endl;
+    cout << "Add 1 to dereferenced val: " << *arr + 1 << endl;
+    cout << "Add 1 and dereference: " << *(arr + 1) << endl;
+}
+
+int main() 
+{
+    simplePointers();
+    cout << endl;
+
+    arrayPointers();
+}
+
