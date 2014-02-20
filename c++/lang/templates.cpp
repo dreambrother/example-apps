95a45792648cfa1d928847a91b988b40e256ba1e
dreambrother <nikshmakov@gmail.com>
2014-02-21 02:35:54 +0400

    add templates example

---
+++ c++/lang/templates.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+template <typename T>
+class Container
+{
+    private:
+    T* value;
+
+    public:
+    Container(T val)
+    {
+        this->value = &val;
+    }
+    T getValue() { return *value; }
+};
+
+int main()
+{
+    Container<int> foo(5);
+    Container<string> bar("bar");
+
+    cout << foo.getValue() << endl;
+    cout << bar.getValue() << endl;
+}
+
