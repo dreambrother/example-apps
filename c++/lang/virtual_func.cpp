2c4243dd7e013d921ca135d3a93b84be2cd5bfac
dreambrother <nikshmakov@gmail.com>
2014-02-21 01:00:33 +0400

    add virtual functions example

---
+++ c++/lang/virtual_func.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class NotVirtBase
+{
+    public:
+    int getFoo() { return 5; }
+};
+
+class NotVirtDerived
+{
+    public:
+    int getFoo() { return 10; }
+};
+
+void testSimpleNotVirtCalls()
+{
+   NotVirtBase* base = new NotVirtBase();
+   cout << base->getFoo() << endl;
+
+   NotVirtDerived* derived = new NotVirtDerived();
+   cout << derived->getFoo() << endl;
+
+   delete base;
+   delete derived;
+}
+
+void testAssignedNotVirtCalls()
+{
+    NotVirtBase* base = (NotVirtBase*)(new NotVirtDerived());
+    cout << base->getFoo() << endl;
+
+    delete base;
+}
+
+class VirtBase
+{
+    public:
+    virtual int getFoo() { return 5; }
+};
+
+class VirtDerived
+{
+    public:
+    virtual int getFoo() { return 10; }
+};
+
+void testVirtCalls()
+{
+    VirtBase* base = (VirtBase*)(new VirtDerived());
+    cout << base->getFoo() << endl;
+
+    delete base;
+}
+
+int main()
+{
+    testSimpleNotVirtCalls();
+    testAssignedNotVirtCalls();
+    testVirtCalls();
+}
