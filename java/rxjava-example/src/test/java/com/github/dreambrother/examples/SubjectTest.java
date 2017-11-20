ff7b3bec5c6640ac075d1dff3d0968df853bc58f
Shmakov Nikita <nshmakov@alfabank.ru>
2017-11-20 14:26:06 +0300

    testSubjectWithEmitter

--- java/rxjava-example/src/test/java/com/github/dreambrother/examples/SubjectTest.java
+++ java/rxjava-example/src/test/java/com/github/dreambrother/examples/SubjectTest.java
@@ -2,8 +2,14 @@ package com.github.dreambrother.examples;
 
 import com.github.dreambrother.examples.subject.EventsGenerator;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
+import rx.Emitter;
+import rx.Observable;
 import rx.subjects.PublishSubject;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class SubjectTest {
@@ -15,7 +21,22 @@ public class SubjectTest {
         subject.subscribe(val -> log.info("Second subscriber got: {}", val));
 
         new EventsGenerator(subject).start();
+        // 1 thread will be created
 
         Thread.sleep(5000L);
+    }
+
+    @Test
+    public void testSubjectWithEmitter() throws InterruptedException {
+        Observable<String> observable = PublishSubject.create((Emitter<String> emitter) -> {
+            Executors.newSingleThreadScheduledExecutor()
+                    .scheduleAtFixedRate(() -> emitter.onNext(RandomStringUtils.randomAlphabetic(5)), 0, 1, TimeUnit.SECONDS);
+        }, Emitter.BackpressureMode.DROP);
+
+        observable.subscribe(log::info);
+        observable.subscribe(log::info);
+        // 2 threads will be created
+
+        Thread.sleep(5000l);
     }
 }
