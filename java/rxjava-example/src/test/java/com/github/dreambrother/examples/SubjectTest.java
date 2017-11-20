e32b499dc730c4687a5a5b2fae0599ebad127185
Shmakov Nikita <nshmakov@alfabank.ru>
2017-11-20 17:29:51 +0300

    test refCount

--- java/rxjava-example/src/test/java/com/github/dreambrother/examples/SubjectTest.java
+++ java/rxjava-example/src/test/java/com/github/dreambrother/examples/SubjectTest.java
@@ -1,14 +1,18 @@
 package com.github.dreambrother.examples;
 
 import com.github.dreambrother.examples.subject.EventsGenerator;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import rx.Emitter;
 import rx.Observable;
+import rx.Subscription;
 import rx.subjects.PublishSubject;
+import rx.subscriptions.Subscriptions;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -28,15 +32,43 @@ public class SubjectTest {
 
     @Test
     public void testSubjectWithEmitter() throws InterruptedException {
-        Observable<String> observable = PublishSubject.create((Emitter<String> emitter) -> {
-            Executors.newSingleThreadScheduledExecutor()
-                    .scheduleAtFixedRate(() -> emitter.onNext(RandomStringUtils.randomAlphabetic(5)), 0, 1, TimeUnit.SECONDS);
-        }, Emitter.BackpressureMode.DROP);
+        Observable<String> observable = PublishSubject.create((Emitter<String> emitter) ->
+                Executors.newSingleThreadScheduledExecutor()
+                        .scheduleAtFixedRate(() -> emitter.onNext(RandomStringUtils.randomAlphabetic(5)), 0, 1, TimeUnit.SECONDS), Emitter.BackpressureMode.DROP);
 
         observable.subscribe(log::info);
         observable.subscribe(log::info);
         // 2 threads will be created
 
-        Thread.sleep(5000l);
+        Thread.sleep(5000L);
+    }
+
+    @Test
+    public void testPublishWithRefCount() throws InterruptedException {
+        Observable<String> observable = Observable.unsafeCreate(subscriber -> {
+            log.info("Creating observable");
+            ScheduledFuture<?> scheduledFuture = Executors.newSingleThreadScheduledExecutor()
+                    .scheduleAtFixedRate(() -> subscriber.onNext(RandomStringUtils.randomAlphanumeric(5)), 0, 1, TimeUnit.SECONDS);
+            subscriber.add(Subscriptions.create(() -> scheduledFuture.cancel(true)));
+        }).publish()
+                .refCount()
+                .map(String::valueOf);
+
+        Subscription subscription1 = observable.subscribe(log::info);
+        Subscription subscription2 = observable.subscribe(log::info);
+        // 1 thread will be created
+
+        Thread.sleep(5000L);
+
+        subscription1.unsubscribe();
+        subscription2.unsubscribe();
+        log.info("Unsubscribed");
+
+        Thread.sleep(2000L);
+
+        observable.subscribe(log::info);
+        // new pool will be created
+
+        Thread.sleep(2000L);
     }
 }
